59419304b92f3eb955a1f589e6534f4759a119cc
Suleymanov Andrey <sasus37@gmail.com>
2022-08-12 17:36:07 +0300

    done

--- main.cpp
+++ main.cpp
@@ -24,16 +24,15 @@ int main() {
     std::tm nowTime = *std::localtime (&t);
     time_t timeOffset = (timer.tm_min*60 + timer.tm_sec);
     time_t EndTime = time (nullptr) + timeOffset;
-    EndTime -= nowTime.tm_min*60 + nowTime.tm_sec;
+
 
     for (int i =0 ; ; i++) {
-        time_t temp = time (nullptr);
-        time_t zero = temp - (nowTime.tm_min*60 + nowTime.tm_sec);
+        time_t zero =  time (nullptr);
         if(zero - EndTime == 0) {cout << "DING! DING! DING!\n";Sleep(1000);break;}
-        time_t Offset = EndTime - (EndTime - zero);
+        time_t Offset = EndTime - zero;
         tm* Counter = std::localtime (&Offset);
         cout << put_time(Counter, "%M:%S");
-        Sleep(500);
+        Sleep(333);
         system("cls");
     }
 }
