1ee199c7e99aeef04c093d3389814a79b7677f12
Suleymanov Andrey <sasus37@gmail.com>
2022-08-12 17:04:45 +0300

    int / minsec

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <iomanip>
+#include <Windows.h>
+using namespace std;
+
+bool overflow ( ){
+
+    if (! std::cin){
+        std::cout <<"Input error.Enter again: ";
+        std::cin.clear();
+        std::cin.ignore(1000, '\n');
+
+        return true;
+    } else {return false;}
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    time_t t = time (nullptr);
+    std::tm timer = *std::localtime (&t);
+    cout << "Enter timer values";
+    cin >> get_time (&timer,"%M:%S");
+    while (overflow ()){cin >> get_time (&timer,"%M:%S");}
+
+    std::tm nowTime = *std::localtime (&t);
+    time_t timeOffset = (timer.tm_min*60 + timer.tm_sec);
+    time_t EndTime = time (nullptr) + timeOffset;
+
+
+    for (int i = (int) timeOffset ; ; i--) {
+        time_t temp = time (nullptr);
+        if(temp - EndTime == 0) {cout << "DING! DING! DING!\n";Sleep(2000);break;}
+        int min =  ((int) timeOffset -i) /60;
+        int sec = ((int) timeOffset-i)%60;
+        cout << min << ":" << sec;
+        Sleep(900);
+        system("cls");
+    }
 }
